1af604b4fc7027d05ce8018319a529abeb2304ab
Michel Sebaaly <michelsebaaly4@gmail.com>
2024-12-09 22:15:40 +0200

    C2

---
+++ index.js
@@ -0,0 +1,22 @@
+let employees = [];
+
+fetch("http://localhost:3000/employees")
+  .then((response) => response.json())
+  .then((json) =>
+    json.forEach((employee) => {
+      employees.push(employee);
+    })
+  );
+
+function displayEmployees() {
+  let tableEmployees = document.getElementById("employees");
+  employees.forEach((employee) => {
+    let tableRow = document.createElement("tr");
+    ["ID", "Name", "Lastname", "Phone", "Address"].forEach((column) => {
+      const tableData = document.createElement("td");
+      tableData.innerHTML = employee[column];
+      tableRow.appendChild(tableData);
+    });
+    tableEmployees.appendChild(tableRow);
+  });
+}
